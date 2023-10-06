5b5daa2de886ffbb2eb21401087e62495360d800
Caio Melo <c.melo92@gmail.com>
2023-10-06 11:42:00 -0300

    Add condition for initial pinecone setup. Add while loop for continuous prompting. Add break when user types exit or quit. Add system message

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import openai
 import pinecone
-import sys
 from dotenv import load_dotenv, find_dotenv
 from langchain.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -9,37 +8,52 @@ from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
+from langchain import PromptTemplate
 
 _ = load_dotenv(find_dotenv())
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 # make setup happen based on conditional (setup is lines 17-30)
-loader = PyPDFDirectoryLoader("data/")
-docs = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 1500,
-    chunk_overlap = 150
-)
-
-splits = text_splitter.split_documents(docs)
-
-pinecone.init(api_key=os.environ["PINECONE_API_KEY"], 
-                environment=os.environ["PINECONE_ENVIRONMENT"])
-
-print("OpenAI and Pinecone successfully initialized!")
+initial_setup = False
 
 embedding = OpenAIEmbeddings()
 # Initialize Pinecone index
-indexName = os.environ["PINECONE_INDEX_NAME"]
-index = pinecone.Index(index_name=indexName)
+index_name = os.environ["PINECONE_INDEX_NAME"]
+index = pinecone.Index(index_name=index_name)
 vectordb = Pinecone(index=index, embedding=embedding, text_key="text")
 
-vectordb.add_documents(splits) # only need to run this once!
+if initial_setup:
+    loader = PyPDFDirectoryLoader("data/")
+    docs = loader.load()
 
-# put prompt - print response in a while loop (lines 41-end of file)
-question = input("\nPrompt: ")
-docs = vectordb.similarity_search(question,k=3)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
+
+    splits = text_splitter.split_documents(docs)
+
+    pinecone.init(
+        api_key=os.environ["PINECONE_API_KEY"],
+        environment=os.environ["PINECONE_ENVIRONMENT"],
+    )
+
+    vectordb.add_documents(splits)  # only need to run this once!
+
+    print("Documents successfully added to Pinecone!")
+
+prompt_template = """
+    1. You're an assistant that answers informations only about Dracula and Frankenstein. Don't answer questions about cookies or anything else.
+
+    {context}
+
+    The queries are delimited by ####
+
+    ####{question}####
+"""
+
+prompt = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+chain_type_kwargs = {"prompt": prompt}
 
 llm_name = "gpt-3.5-turbo-0301"
 llm = ChatOpenAI(model_name=llm_name, temperature=0)
@@ -47,16 +61,24 @@ llm = ChatOpenAI(model_name=llm_name, temperature=0)
 qa_chain = RetrievalQA.from_chain_type(
     llm,
     retriever=vectordb.as_retriever(),
-    return_source_documents=True
+    return_source_documents=True,
+    chain_type_kwargs=chain_type_kwargs,
 )
 
-result = qa_chain({"query": question})
+print("OpenAI and Pinecone successfully initialized!")
 
-print(result["result"])
+while True:
+    question = input("\nPrompt: ")
 
-source_document = result["source_documents"][0]
-print(source_document.metadata)
+    if question == "quit" or question == "exit":
+        print("Exitting the prompt as requested.")
+        break
 
-# add a system message
-# add some instructions for the bot to follow (i.e. you're a bot about dracula and frankenstein - don't answer questions about cookies
-# add a way to exit the loop (i.e. type "exit" or "quit")
+    docs = vectordb.similarity_search(question, k=3)
+
+    result = qa_chain(question)
+
+    print(result["result"])
+
+    source_document = result["source_documents"][0]
+    print(source_document.metadata)
